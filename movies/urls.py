b581200be47e562d490709532c8274a6087210ff
Levian-Liu-Guanyi <zhiweisdsz@hotmail.com>
2026-03-07 06:50:09 +0800

    Update urls.py
    Updates for user story 2

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path('', views.index, name='movies.index'),
     path('<int:id>/', views.show, name='movies.show'),
     path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
+    path('<int:id>/rating/create/', views.create_rating, name='movies.create_rating'),
     path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
     path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='movies.delete_review'),
     path('<int:id>/review/<int:review_id>/report/', views.report_review, name='movies.report_review'),
